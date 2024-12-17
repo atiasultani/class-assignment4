35d925d1bf6cc96b5f0626ff3ee9bf5091186a96
atiasultani <160133182+atiasultani@users.noreply.github.com>
2024-12-18 03:13:47 +0500

    grid

--- grid/src/app/page.tsx
+++ grid/src/app/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
       className='div1 bg-yellow-400 w-[120px] h-[500px] gap-x-8 gap-y-8 outline outline-white'>Nav</div>
       <div className= 'div2 -ml-[32em] bg-yellow-400 w-[820px] h-20 gap-x-8 gap-y-8 outline outline-white'> Header</div>
       <div className='div3 ml-32 -mt-[25em]  bg-yellow-400 w-[700px] h-[350px] gap-x-8 gap-y-8 outline outline-white'>cont1</div>
-      <div className='div4 ml-48 -mt-[25em] bg-yellow-400 w-[120px] h-[400px] gap-x-8 gap-y-8 outline outline-white'>cont2</div>
+      <div className='div4 ml-48 -mt-[25em] bg-yellow-400 w-[120px] h-[400px] gap-x-8 gap-y-8 outline outline-white'>Ads</div>
       <div className='div5 ml-32 -mt-[33em] bg-yellow-400 w-[695px] h-12 gap-x-8 gap-y-8 outline outline-white'>Footer</div>
 
 
