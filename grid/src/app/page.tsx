785767bdc583d321965287caa308133182068599
atiasultani <160133182+atiasultani@users.noreply.github.com>
2024-12-19 01:01:37 +0500

    grid

--- grid/src/app/page.tsx
+++ grid/src/app/page.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='grid grid-cols-2 grid-rows-3 px-7 py-8'>
+    <div className='h-screen grid grid-cols-6 grid-rows-6 px-7 py-8'>
       
+      <div className= 'bg-yellow-400 outline outline-white row-span-7'> Nav </div>
       <div 
-      className='div1 bg-yellow-400 w-[120px] h-[500px] gap-x-8 gap-y-8 outline outline-white'>Nav</div>
-      <div className= 'div2 -ml-[32em] bg-yellow-400 w-[820px] h-20 gap-x-8 gap-y-8 outline outline-white'> Header</div>
-      <div className='div3 ml-32 -mt-[25em]  bg-yellow-400 w-[700px] h-[350px] gap-x-8 gap-y-8 outline outline-white'>cont1</div>
-      <div className='div4 ml-48 -mt-[25em] bg-yellow-400 w-[120px] h-[400px] gap-x-8 gap-y-8 outline outline-white'>Ads</div>
-      <div className='div5 ml-32 -mt-[33em] bg-yellow-400 w-[695px] h-12 gap-x-8 gap-y-8 outline outline-white'>Footer</div>
+      className='bg-yellow-400 outline outline-white col-span-5'>Header</div>
+      
+      <div className=' bg-yellow-400 outline outline-white col-span-4 row-span-4'>Article</div>
+      <div className=' bg-yellow-400 outline outline-white row-span-4'>Ads</div>
+      <div className=' bg-yellow-400 outline outline-white col-span-5 row-span-5'>Footer</div>
 
 
     </div>
